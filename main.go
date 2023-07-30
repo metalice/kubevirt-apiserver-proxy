1d4717748a05be86b4b365a87daabb208324dc86
Matan Schatzman <mschatzm@redhat.com>
2023-07-30 14:42:32 +0300

    Adding cache
    Signed-off-by: Matan Schatzman <mschatzm@redhat.com>

--- main.go
+++ main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"log"
+	"time"
 
+	cache "github.com/chenyahui/gin-cache"
+	"github.com/chenyahui/gin-cache/persist"
 	"github.com/gin-gonic/gin"
 	"github.com/kubevirt-ui/kubevirt-apiserver-proxy/handlers"
 )
 
 func main() {
 	server := gin.Default()
-	server.GET("/health", handlers.HealthHandler)
-	server.GET("/apis/*path", handlers.RequestHandler)
-
+	memoryStore := persist.NewMemoryStore(1 * time.Minute)
+	server.GET("/health", cache.CacheByRequestURI(memoryStore, 5*time.Second), handlers.HealthHandler)
+	server.GET("/apis/*path", cache.CacheByRequestURI(memoryStore, 2*time.Second), handlers.RequestHandler)
 	log.Println("listening for server 8080")
 
 	err := server.RunTLS(":8080", "./cert/tls.crt", "./cert/tls.key") // listen and serve on 0.0.0.0:8080
